e1123201e885326e9d6a80d6c5af12d9cc7d8ed7
Alumno <Alumno@L30-2543307>
2022-08-24 12:05:39 -0500

    Agregar archivos de proyecto.

---
+++ Expendedora.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+
+namespace ExpendedoraBase
+{
+    class Expendedora
+    {
+        string marca;
+        byte temperatura;
+        string codigo;
+        float precio;
+
+        public Expendedora()
+        {
+            Saludar();
+            marca = "AMS";
+            Console.WriteLine("Marca: " + marca);
+            ControlarTiempoDisplay();
+            temperatura = 14;
+            Console.WriteLine("La temperatura es: " + temperatura + "°C");
+            ControlarTiempoDisplay();
+            codigo = MostrarCodigoDeProductos();
+            ControlarTiempoDisplay();
+            MostrarPrecio(codigo);
+
+
+        }
+
+        void ControlarTiempoDisplay()
+        {
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        void Saludar()
+        {
+            Console.WriteLine("Bienvenido");
+        }
+
+        string MostrarCodigoDeProductos()
+        {
+            Console.WriteLine("A1:Kinder Delice \tB2:Takis \tC3: Donitas");
+            Console.WriteLine("Ingrese el codigo del prodcuto a elegir");
+            string codigo = Console.ReadLine(); //"Codigo" es una variable local
+            return codigo;
+        }
+
+        void MostrarPrecio(string codigo)
+        {
+            switch (codigo)
+            {
+                case "A1":
+                    Console.WriteLine("El precio es: {0}", precio);
+                    break;
+                case "B2":
+                    Console.WriteLine("El precio es: {0}", precio);
+                    break;
+                case "C3":
+                    Console.WriteLine("El precio es: {0}", precio);
+                    break;
+                default:
+                    Console.WriteLine("Producto no reconocido");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ExpendedoraBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expendedora miExpendedora = new Expendedora();
+            Console.ReadLine();
+
+
+        }
+    }
+}
